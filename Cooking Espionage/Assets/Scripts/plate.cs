73a73cadc8833b8a941434dc3e3950b93b64431d
Pluto <ryan.morrell@gmail.com>
2024-10-24 01:07:40 +0100

    things go on plates

--- Cooking Espionage/Assets/Scripts/plate.cs
+++ Cooking Espionage/Assets/Scripts/plate.cs
@@ -6,8 +6,8 @@ public class plate : MonoBehaviour
 {
     private CapsuleCollider foodPickupZone;
     private bool plateFull = false;
-    private bool movine = false;
-    private bool heldObject;
+    private bool movingFood = false;
+    private Collider heldObject;
 
     // Start is called before the first frame update
     void Start()
@@ -18,26 +18,35 @@ public class plate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (movingFood)
+        {
+            moveFood();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (!plateFull && other.tag == "Finished_Food")
         {
-            other.gameObject.transform.root.parent = this.gameObject.transform;
-            other.TryGetComponent<Rigidbody>(out Rigidbody objectRigidBody);
-            objectRigidBody.MovePosition(this.transform.GetChild(0).position);
-            Destroy(objectRigidBody);
-            other.TryGetComponent(out ObjectGrabbable objectGrabbable);
-            objectGrabbable.Grab(null);
-
+            heldObject = other;
+            movingFood = true;
+            plateFull = true;
 
         }
     }
-
-    private void moveToCenter()
+    private void moveFood()
     {
+        heldObject.transform.position = Vector3.MoveTowards(heldObject.transform.position, this.transform.GetChild(0).position, 0.5f);
+        if (heldObject.transform.position== this.transform.GetChild(0).position)
+        {
+            movingFood = false;
+            heldObject.TryGetComponent(out Rigidbody objectRigidbody);
+            Destroy(objectRigidbody);
+            heldObject.gameObject.transform.root.parent = this.gameObject.transform;
+            heldObject.TryGetComponent(out ObjectGrabbable objectGrabbable);
+            objectGrabbable.Grab(null);
+        }
 
     }
+
 }
